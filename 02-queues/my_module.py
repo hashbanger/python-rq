0ffc60e4e021c2b360d45aac4c3c4dad751d6fdb
hashbanger <prashant.brahmbhatt32@outlook.com>
2021-10-09 13:59:35 +0530

    added new functions

--- 02-queues/my_module.py
+++ 02-queues/my_module.py
@@ -1,8 +1,17 @@
+import time
 import requests
 
 def count_words_at_url(url):
     resp = requests.get(url)
     return len(resp.text.split())
 
-def say_hello():
-    return 'Hello from the job!'
+def say_hello(name=""):
+        return f'Hello {name} from the job!'
+
+def doomed_to_fail(ttl=10, description=None):
+    time.sleep(ttl)
+
+    if not description:
+        return "This statement won't be returned if job timeout < 10 seconds."
+    else:
+        return description
