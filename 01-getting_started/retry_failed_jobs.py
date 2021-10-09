a69b683b67656d7d24bb6470aa2a68ee837ddf71
hashbanger <prashant.brahmbhatt32@outlook.com>
2021-10-09 13:59:13 +0530

    corrected argument position

--- 01-getting_started/retry_failed_jobs.py
+++ 01-getting_started/retry_failed_jobs.py
@@ -8,4 +8,4 @@ q = Queue(connection=Redis())
 result = q.enqueue(say_hello, retry=Retry(max=3))
 
 # Retrying the job upto three times at certain intervals
-result = q.enqueue(say_hello, retry=Retry(max=3), interval=[10, 20, 30])
+result = q.enqueue(say_hello, retry=Retry(max=3, interval=[10, 20, 30]))
